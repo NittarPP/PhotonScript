52047e0d45e7ad5480741f9ff695a4ee73f334bb
Kokie <167613469+NittarPP@users.noreply.github.com>
2025-03-16 20:00:50 +0700

    Update ban.lua

--- ban.lua
+++ ban.lua
@@ -1,4 +1,6 @@
 return {
-    ["PHOTONTESTER"] = "Unsupport user",
-    ["Reason With ledge"] = "Resell"
+    ["NOOBFDAB"] = "Cheating",
+    [2308948481] = "Exploiting",
+    ["49.229.238.89"] = "IP Ban",
+    ["5680FBAC-7760-4593-883D-01468B29A136"] = "Client Ban"
 }
