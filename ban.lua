6abe0cffe9f794a74ec09b71cf0bdf30253e6e1d
Kokie <167613469+NittarPP@users.noreply.github.com>
2025-03-16 20:53:07 +0700

    Update ban.lua

--- ban.lua
+++ ban.lua
@@ -1,6 +1,6 @@
 return {
     ["ROBLOX"] = "Cheating",
     [11111111111] = "Exploiting",
-    ["49.229.238.89"] = "IP Ban",
-    ["5680FBAC-7760-4593-883D-01468B29A136"] = "Client Ban"
+    ["00.000.000.00"] = "IP Ban",
+    ["NIGGA"] = "Client Ban"
 }
