cd90876801e0cd0329521c7c205c428cfb6df475
EqHubv2 <eqhubv2@outlook.com>
2025-04-08 18:09:39 +0700

    Update 111607867914638.lua

--- Src/111607867914638.lua
+++ Src/111607867914638.lua
@@ -1,3 +1,7 @@
+local Library = loadstring(game:HttpGetAsync("https://github.com/ActualMasterOogway/Fluent-Renewed/releases/latest/download/Fluent.luau"))()
+local SaveManager = loadstring(game:HttpGetAsync("https://raw.githubusercontent.com/ActualMasterOogway/Fluent-Renewed/master/Addons/SaveManager.luau"))()
+local InterfaceManager = loadstring(game:HttpGetAsync("https://raw.githubusercontent.com/ActualMasterOogway/Fluent-Renewed/master/Addons/InterfaceManager.luau"))()
+ 
 Library:Notify{
     Title = "Bruh",
     Content = "We didn't make script in Lobby",
