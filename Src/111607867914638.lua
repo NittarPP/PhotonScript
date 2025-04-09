8d4523ec319aaf1d76e94621e47a28b6b3bca1e3
EqHubv2 <eqhubv2@outlook.com>
2025-04-09 17:29:33 +0700

    Update 111607867914638.lua

--- Src/111607867914638.lua
+++ Src/111607867914638.lua
@@ -2,6 +2,28 @@ local Library = loadstring(game:HttpGetAsync("https://github.com/ActualMasterOog
 local SaveManager = loadstring(game:HttpGetAsync("https://raw.githubusercontent.com/ActualMasterOogway/Fluent-Renewed/master/Addons/SaveManager.luau"))()
 local InterfaceManager = loadstring(game:HttpGetAsync("https://raw.githubusercontent.com/ActualMasterOogway/Fluent-Renewed/master/Addons/InterfaceManager.luau"))()
  
+local Window = Library:CreateWindow{
+    Title = "E.R.P.O (In game)",
+    SubTitle = "by 21xp Team",
+    TabWidth = 160,
+    Size = UDim2.fromOffset(830, 525),
+    Resize = true,
+    MinSize = Vector2.new(470, 380),
+    Acrylic = false,
+    Theme = "Dark",
+    MinimizeKey = Enum.KeyCode.RightControl
+}
+local Tabs = {
+    Main = Window:CreateTab{
+        Title = "Visual",
+        Icon = "eye"
+    }
+}
+Tabs.Main:CreateParagraph("Aligned Paragraph", {
+    Title = "We not support in Lobby",
+    TitleAlignment = "Middle",
+    ContentAlignment = Enum.TextXAlignment.Center
+})
 Library:Notify{
     Title = "Bruh",
     Content = "We didn't make script in Lobby",
