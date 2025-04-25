137097b7cbc46641854b30692179d007548fab5f
Kokie <167613469+NittarPP@users.noreply.github.com>
2025-04-25 22:50:42 +0700

    Update ban.lua

--- ban.lua
+++ ban.lua
@@ -1,5 +1,6 @@
 return {
     ["45.132.84.1"] = "Try Bypass Key but why",
     ["36.78.244.229"] = "Try Bypass Key but why",
-    ["agawarmas321"] = "Stupid Cringe Thai Kid"
+    ["agawarmas321"] = "Stupid Cringe Thai Kid",
+    ["ylian214000"] = "Nah uh"
 }
